6beb55550eda122abe6fce8f1a664ee5230ae3fd
manavmalavia18 <114517641+manavmalavia18@users.noreply.github.com>
2026-05-24 16:20:16 -0400

    fix: terraform hcl syntax - multi-line blocks

--- terraform/aws/cluster/modules/eks/variables.tf
+++ terraform/aws/cluster/modules/eks/variables.tf
@@ -1,7 +1,27 @@
-variable "project_name" { type = string }
-variable "cluster_version" { type = string }
-variable "subnet_ids" { type = list(string) }
-variable "node_instance_type" { type = string }
-variable "node_count" { type = number }
-variable "node_count_min" { type = number }
-variable "node_count_max" { type = number }
+variable "project_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "node_instance_type" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "node_count_min" {
+  type = number
+}
+
+variable "node_count_max" {
+  type = number
+}
--- terraform/aws/cluster/variables.tf
+++ terraform/aws/cluster/variables.tf
@@ -1,10 +1,49 @@
-variable "aws_region" { type = string, default = "us-east-1" }
-variable "aws_account_id" { type = string }
-variable "project_name" { type = string, default = "jobradar" }
-variable "cluster_version" { type = string, default = "1.34" }
-variable "node_instance_type" { type = string, default = "t3.small" }
-variable "node_count" { type = number, default = 3 }
-variable "node_count_min" { type = number, default = 1 }
-variable "node_count_max" { type = number, default = 5 }
-variable "grafana_password" { type = string, sensitive = true, default = "jobradar123" }
-variable "argocd_password" { type = string, sensitive = true }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
+variable "project_name" {
+  type    = string
+  default = "jobradar"
+}
+
+variable "cluster_version" {
+  type    = string
+  default = "1.34"
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.small"
+}
+
+variable "node_count" {
+  type    = number
+  default = 3
+}
+
+variable "node_count_min" {
+  type    = number
+  default = 1
+}
+
+variable "node_count_max" {
+  type    = number
+  default = 5
+}
+
+variable "grafana_password" {
+  type      = string
+  sensitive = true
+  default   = "jobradar123"
+}
+
+variable "argocd_password" {
+  type      = string
+  sensitive = true
+}
