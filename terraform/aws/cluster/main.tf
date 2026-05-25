6234f31af9733a2a0a50d2fd2d40585f6e0c6e89
manavmalavia18 <114517641+manavmalavia18@users.noreply.github.com>
2026-05-25 11:07:12 -0400

    fix: align cluster terraform state and externaldns config

--- terraform/aws/cluster/main.tf
+++ terraform/aws/cluster/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    aws    = { source = "hashicorp/aws", version = "~> 5.0" }
-    helm   = { source = "hashicorp/helm", version = "~> 2.0" }
-    null   = { source = "hashicorp/null", version = "~> 3.0" }
+    aws  = { source = "hashicorp/aws", version = "~> 5.0" }
+    helm = { source = "hashicorp/helm", version = "~> 2.0" }
+    null = { source = "hashicorp/null", version = "~> 3.0" }
   }
 }
 
--- terraform/aws/cluster/outputs.tf
+++ terraform/aws/cluster/outputs.tf
@@ -1,15 +1,7 @@
-output "ecr_repository_url" {
-  value = data.aws_ecr_repository.api.repository_url
-}
-
 output "cluster_name" {
-  value = module.eks.cluster_name
+  value = var.project_name
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
 }
